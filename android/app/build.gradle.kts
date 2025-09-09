e4e95d699ea951883109b5b3806f01e72313bd43
Wens <phawenslouisjean9@gmail.com>
2025-09-09 10:53:40 -0700

    creation app bundle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
+    // Tous les plugins doivent être dans un seul bloc en haut du fichier.
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Le plugin Flutter doit être appliqué après les plugins Android et Kotlin.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+}
+
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -31,15 +41,23 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Un seul `signingConfig` est nécessaire pour la version de publication.
+            // La ligne `signingConfig = signingConfigs.getByName("debug")` est inutile ici.
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
-
 
 flutter {
     source = "../.."
